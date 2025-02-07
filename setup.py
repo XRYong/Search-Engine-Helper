1923addfc52679c590f2245f10de84103c329a52
X. R. Yong <xuanruiyong2012@gmail.com>
2025-02-07 19:29:25 +0800

    Update setup.py
    working setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
+import JSON
+
 print("Setup Search Engine Helper")
 print("Coded by @XRYong on GitHub")
 print("https://xryong.github.io")
 print("Setup API Keys for services")
+
+google_key = input("Enter Google API key: ")
+facebook_key = input("Enter Facebook API key: )
+twitter_key = input("Enter X (formally Twitter) API Key")
+
+api_keys = {
+ "google": google_key
+ "twitter": twitter_key
+ "Facebook": facebook_key
+}
+
+
+with open("config.json" as "w") as json_file:
+ json.dump(api_keys, json_file)
